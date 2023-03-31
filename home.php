11ba6af776bbdb2eb20c7ae22d7e38f93365242e
douglaskks <douglaszxv2@gmail.com>
2023-03-31 02:14:58 -0300

    Tela Home Criada

---
+++ home.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Inicial</title>
+
+    <style>
+
+    body{
+            font-family: Arial, Helvetica, sans-serif;
+            background-image: linear-gradient(to right, rgb(150, 20, 220), rgb(67, 1, 102));
+            text-align: center;
+        }
+    .box{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background-color: rgba(0, 0, 0, 0.6);
+        padding: 30px;
+        border-radius: 15px;
+    }
+
+    a{
+        text-decoration: none;
+        color: white;
+        border: 1px solid #b714dc;
+        padding: 10px;
+        border-radius: 10px;
+    }
+
+    a:hover{
+        background-color: #b714dc;
+    }
+    </style>
+</head>
+<body>
+    <h1>O login teve sucesso!</h1>
+
+    <div class="box">
+        <a href="">Teste</a>
+        <a href="">Teste</a>
+    </div>
+</body>
+</html>
